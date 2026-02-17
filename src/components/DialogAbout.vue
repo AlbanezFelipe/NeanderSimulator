be9b2f70b5320be261f70b342ba46c5e59259cc7
albanez <albanez@localhost.localdomain>
2026-02-16 22:49:04 -0300

    add License link

--- src/components/DialogAbout.vue
+++ src/components/DialogAbout.vue
@@ -9,7 +9,7 @@
                 <p>O <b class="text-primary">Simulador Computador Neander</b> foi projetado com o intuito de continuar e aprimorar o trabalho original do <a class="text-link-primary" href="https://www.inf.ufrgs.br/site/docente/raul-fernando-weber/" target="_blank">Professor Dr. Raul Weber</a>, redesenhado para aplicações web e mobile com novos recursos inovadores como a diagramação em tempo real da arquitetura executada ciclo a ciclo.</p>
                 <p>Este trabalho foi tema de minha <a class="text-link-primary" href="https://albanezfelipe.github.io/docs/Neander/Contin,%20Felipe%20Albanez.%20Trabalho%20de%20conclus%C3%A3o%20de%20curso%20Engenharia%20de%20Computa%C3%A7%C3%A3o.pdf" target="_blank">Monografia de conclusão de curso</a> com a proposta de desenvolver uma ferramenta didática a fim de facilitar a abstração de conceitos de matérias de engenharia de computação, como arquitetura de computadores, e reforçar a prática do estudo.</p>
                 <span style="font-size: 12px; color: rgba(0, 0, 0, 0.54);" class="block text-center q-pb-xs">Developed by <a class="text-link" href="https://albanezfelipe.github.io/" target="_blank">Felipe Albanez Contin</a></span>
-                <span style="font-size: 12px; color: rgba(0, 0, 0, 0.54);" class="block text-center">This project is under the <a class="text-link" href="#" target="_blank">General Public License</a></span>
+                <span style="font-size: 12px; color: rgba(0, 0, 0, 0.54);" class="block text-center">This project is under the <a class="text-link" href="https://github.com/AlbanezFelipe/NeanderSimulator/blob/master/LICENSE" target="_blank">General Public License</a></span>
             </q-card-section>
 
             <q-card-actions align="right">
